60937e75ef6d616610489a2b6858fa247df0f0d3
Abdul Aziz Muhammad Abdul Majid <aazizmajid2@gmail.com>
2024-08-06 17:42:27 +0300

    Update customEvents.js

--- js/customEvents.js
+++ js/customEvents.js
@@ -1,17 +1,26 @@
 var iardaCalendarEvents = [
       {
-        id: 'bHay68s', // Event's ID (required)
-        name: "New Year", // Event name (required)
+        id: '06082024-1', // Event's ID (required)
+        name: "هنالك العديد من الأنواع المتوفرة لنصوص لوريم إيبسوم", // Event name (required)
         date: "August/6/2024", // Event date (required)
         type: "holiday", // Event type (required)
-        everyYear: true, // Same event every year (optional)
+        everyYear: false, // Same event every year (optional)
+		color: "#EF9700" // Event custom color (optional)
+      },
+	  {
+        id: '06082024-2', // Event's ID (required)
+        name: "هنالك العديد من الأنواع المتوفرة لنصوص لوريم إيبسوم", // Event name (required)
+        date: "August/6/2024", // Event date (required)
+        type: "birthday", // Event type (required)
+        everyYear: false, // Same event every year (optional)
 		color: "#EF9700" // Event custom color (optional)
       },
       {
-        name: "Vacation Leave",
-        badge: "02/13 - 02/15", // Event badge (optional)
+		  id: '08082024-1', // Event's ID (required)
+        name: "لوريم إيبسوم",
+        badge: "08/07 - 08/10", // Event badge (optional)
         date: ["August/7/2024", "August/10/2024"], // Date range
-        description: "Vacation leave for 3 days.", // Event description (optional)
+        description: "هنالك العديد من الأنواع المتوفرة لنصوص لوريم إيبسوم", // Event description (optional)
         type: "event",
         color: "#63d867" // Event custom color (optional)
       }
