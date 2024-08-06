e94f81a46c2fe2f08d9ddba4bed5996c84a3df52
Abdul Aziz Muhammad Abdul Majid <aazizmajid2@gmail.com>
2024-08-06 13:07:33 +0300

    Add files via upload

---
+++ js/customEvents.js
@@ -0,0 +1,18 @@
+var iardaCalendarEvents = [
+      {
+        id: 'bHay68s', // Event's ID (required)
+        name: "New Year", // Event name (required)
+        date: "August/6/2024", // Event date (required)
+        type: "holiday", // Event type (required)
+        everyYear: true, // Same event every year (optional)
+		color: "#EF9700" // Event custom color (optional)
+      },
+      {
+        name: "Vacation Leave",
+        badge: "02/13 - 02/15", // Event badge (optional)
+        date: ["August/7/2024", "August/10/2024"], // Date range
+        description: "Vacation leave for 3 days.", // Event description (optional)
+        type: "event",
+        color: "#63d867" // Event custom color (optional)
+      }
+    ]
