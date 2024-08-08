2133c7988572e4f965ae1f80b28e60bded0d688a
Abdul Aziz Muhammad Abdul Majid <aazizmajid2@gmail.com>
2024-08-08 11:31:48 +0300

    Update customEvents.js

--- js/customEvents.js
+++ js/customEvents.js
@@ -1,16 +1,16 @@
 var iardaCalendarEvents = [
       {
-        id: '06082024-1', // Event's ID (required)
+        id: '10082024-1', // Event's ID (required)
         name: "هنالك العديد من الأنواع المتوفرة لنصوص لوريم إيبسوم", // Event name (required)
-        date: "August/6/2024", // Event date (required)
+        date: "August/10/2024", // Event date (required)
         type: "holiday", // Event type (required)
         everyYear: false, // Same event every year (optional)
-		color: "#EF9700" // Event custom color (optional)
+		color: "#63d867" // Event custom color (optional)
       },
 	  {
-        id: '06082024-2', // Event's ID (required)
+        id: '10082024-2', // Event's ID (required)
         name: "هنالك العديد من الأنواع المتوفرة لنصوص لوريم إيبسوم", // Event name (required)
-        date: "August/6/2024", // Event date (required)
+        date: "August/10/2024", // Event date (required)
         type: "birthday", // Event type (required)
         everyYear: false, // Same event every year (optional)
 		color: "#EF9700" // Event custom color (optional)
@@ -18,8 +18,8 @@ var iardaCalendarEvents = [
       {
 		  id: '08082024-1', // Event's ID (required)
         name: "لوريم إيبسوم",
-        badge: "08/07 - 08/10", // Event badge (optional)
-        date: ["August/7/2024", "August/10/2024"], // Date range
+        badge: "08/21 - 08/24", // Event badge (optional)
+        date: ["August/21/2024", "August/24/2024"], // Date range
         description: "هنالك العديد من الأنواع المتوفرة لنصوص لوريم إيبسوم", // Event description (optional)
         type: "event",
         color: "#63d867" // Event custom color (optional)
