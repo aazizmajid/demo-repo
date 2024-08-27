c3f10189aeff3b2b73841cb042362e6a90bbc6b0
Abdul Aziz Muhammad Abdul Majid <aazizmajid2@gmail.com>
2024-08-27 10:34:06 +0300

    Update customEvents.js
    Add more Events (Dummy)

--- js/customEvents.js
+++ js/customEvents.js
@@ -1,16 +1,16 @@
 var iardaCalendarEvents = [
       {
-        id: '10082024-1', // Event's ID (required)
+        id: '06082024-1', // Event's ID (required)
         name: "هنالك العديد من الأنواع المتوفرة لنصوص لوريم إيبسوم", // Event name (required)
-        date: "August/10/2024", // Event date (required)
+        date: "August/6/2024", // Event date (required)
         type: "holiday", // Event type (required)
         everyYear: false, // Same event every year (optional)
-		color: "#63d867" // Event custom color (optional)
+		color: "#EF9700" // Event custom color (optional)
       },
 	  {
-        id: '10082024-2', // Event's ID (required)
+        id: '06082024-2', // Event's ID (required)
         name: "هنالك العديد من الأنواع المتوفرة لنصوص لوريم إيبسوم", // Event name (required)
-        date: "August/10/2024", // Event date (required)
+        date: "August/6/2024", // Event date (required)
         type: "birthday", // Event type (required)
         everyYear: false, // Same event every year (optional)
 		color: "#EF9700" // Event custom color (optional)
@@ -18,8 +18,65 @@ var iardaCalendarEvents = [
       {
 		  id: '08082024-1', // Event's ID (required)
         name: "لوريم إيبسوم",
-        badge: "08/21 - 08/24", // Event badge (optional)
-        date: ["August/21/2024", "August/24/2024"], // Date range
+        badge: "08/07 - 08/10", // Event badge (optional)
+        date: ["August/7/2024", "August/10/2024"], // Date range
+        description: "هنالك العديد من الأنواع المتوفرة لنصوص لوريم إيبسوم", // Event description (optional)
+        type: "event",
+        color: "#63d867" // Event custom color (optional)
+      }
+	  {
+        id: '06092024-1', // Event's ID (required)
+        name: "هنالك العديد من الأنواع المتوفرة لنصوص لوريم إيبسوم", // Event name (required)
+        date: "September/6/2024", // Event date (required)
+        type: "holiday", // Event type (required)
+        everyYear: false, // Same event every year (optional)
+		color: "#EF9700" // Event custom color (optional)
+      },
+	    {
+        id: '06092024-2', // Event's ID (required)
+        name: "هنالك العديد من الأنواع المتوفرة لنصوص لوريم إيبسوم", // Event name (required)
+        date: "September/6/2024", // Event date (required)
+        type: "holiday", // Event type (required)
+        everyYear: false, // Same event every year (optional)
+		color: "#63d867" // Event custom color (optional)
+      },
+	  {
+        id: '21092024-3', // Event's ID (required)
+        name: "هنالك العديد من الأنواع المتوفرة لنصوص لوريم إيبسوم", // Event name (required)
+        date: "September/21/2024", // Event date (required)
+        type: "holiday", // Event type (required)
+        everyYear: false, // Same event every year (optional)
+		color: "#EF9700" // Event custom color (optional)
+      },
+	  {
+        id: '28092024-4', // Event's ID (required)
+        name: "هنالك العديد من الأنواع المتوفرة لنصوص لوريم إيبسوم", // Event name (required)
+        date: "September/28/2024", // Event date (required)
+        type: "birthday", // Event type (required)
+        everyYear: false, // Same event every year (optional)
+		color: "#63d867" // Event custom color (optional)
+      }
+	   {
+        id: '05102024-1', // Event's ID (required)
+        name: "هنالك العديد من الأنواع المتوفرة لنصوص لوريم إيبسوم", // Event name (required)
+        date: "October/05/2024", // Event date (required)
+        type: "holiday", // Event type (required)
+        everyYear: false, // Same event every year (optional)
+		color: "#63d867" // Event custom color (optional)
+      },
+	  {
+        id: '16102024-2', // Event's ID (required)
+        name: "هنالك العديد من الأنواع المتوفرة لنصوص لوريم إيبسوم", // Event name (required)
+        date: "October/16/2024", // Event date (required)
+        type: "birthday", // Event type (required)
+        everyYear: false, // Same event every year (optional)
+		color: "#EF9700" // Event custom color (optional)
+      },
+	  {
+		id: '2102024-1', // Event's ID (required)
+        name: "لوريم إيبسوم",
+        badge: "10/21 - 10/28", // Event badge (optional)
+        date: ["October/21/2024", "October/28/2024"], // Date range
         description: "هنالك العديد من الأنواع المتوفرة لنصوص لوريم إيبسوم", // Event description (optional)
         type: "event",
         color: "#63d867" // Event custom color (optional)
